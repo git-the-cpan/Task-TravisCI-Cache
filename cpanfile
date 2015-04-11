b254a1e3379dd40f0430fd36fd20e2c9ab669530
Chris Weyl <rsrchboy@cpan.org>
2015-04-11 21:05:18 +0000

    Import of RSRCHBOY/Task-TravisCI-Cache-0.001.tar.gz from CPAN.
    gitpan-cpan-distribution: Task-TravisCI-Cache
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         RSRCHBOY/Task-TravisCI-Cache-0.001.tar.gz
    gitpan-cpan-author:       RSRCHBOY
    gitpan-cpan-maturity:     released

---
+++ cpanfile
@@ -0,0 +1,32 @@
+requires "DBIx::Class::Schema::Loader" => "0.07042";
+requires "Dist::Zilla::PluginBundle::RSRCHBOY" => "0.055";
+requires "Task::BeLike::RSRCHBOY" => "0.007";
+requires "Text::Wrap" => "2013.0523";
+requires "perl" => "5.006";
+
+on 'test' => sub {
+  requires "File::Spec" => "0";
+  requires "IO::Handle" => "0";
+  requires "IPC::Open3" => "0";
+  requires "Test::CheckDeps" => "0.010";
+  requires "Test::More" => "0.94";
+  requires "perl" => "5.006";
+  requires "strict" => "0";
+  requires "warnings" => "0";
+};
+
+on 'configure' => sub {
+  requires "ExtUtils::MakeMaker" => "0";
+  requires "perl" => "5.006";
+};
+
+on 'develop' => sub {
+  requires "Pod::Coverage::TrustPod" => "0.100003";
+  requires "Test::EOL" => "1.5";
+  requires "Test::More" => "0.88";
+  requires "Test::NoTabs" => "1.4";
+  requires "Test::Pod" => "1.41";
+  requires "Test::Pod::Coverage" => "1.08";
+  requires "Test::Spelling" => "0.12";
+  requires "version" => "0.9901";
+};
---
+++ lib/Task/TravisCI/Cache.pm
@@ -0,0 +1,72 @@
+#
+# This file is part of Task-TravisCI-Cache
+#
+# This software is Copyright (c) 2015 by Chris Weyl.
+#
+# This is free software, licensed under:
+#
+#   The GNU Lesser General Public License, Version 2.1, February 1999
+#
+package Task::TravisCI::Cache;
+our $AUTHORITY = 'cpan:RSRCHBOY';
+# git description: afe1413
+$Task::TravisCI::Cache::VERSION = '0.001';
+
+# ABSTRACT: Packages pulled in when building a Perl cache for TravisCI
+
+!!42;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Task::TravisCI::Cache - Packages pulled in when building a Perl cache for TravisCI
+
+=head1 VERSION
+
+version 0.001
+
+=head1 DESCRIPTION
+
+This task distribution defines the packages installed inside my TravisCI
+cache, at L<https://github.com/RsrchBoy/travis-p5-cache>.
+
+Note that while I say "my ...  cache", that certainly doesn't mean that you
+cannot use it, or that I won't accept pull-requests for the inclusion of
+additional packages (within reason, at least).
+
+This distribution is build using the L<LatestPrereqs plugin|Dist::Zilla::Plugin::LatestPrereqs>,
+so it will always depend on the latest versions of the specified modules at
+the time of creation.
+
+=head1 TASK CONTENTS
+
+=head2 All
+
+=head3 L<Dist::Zilla::PluginBundle::RSRCHBOY>
+
+=head3 L<Text::Wrap> 2013.0523
+
+The installed version appears to cause some random ABEND on the Travis v5.18 image, currently.
+
+=head3 L<Task::BeLike::RSRCHBOY>
+
+=head3 L<DBIx::Class::Schema::Loader>
+
+=head1 AUTHOR
+
+Chris Weyl <cweyl@alumni.drew.edu>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2015 by Chris Weyl.
+
+This is free software, licensed under:
+
+  The GNU Lesser General Public License, Version 2.1, February 1999
+
+=cut
